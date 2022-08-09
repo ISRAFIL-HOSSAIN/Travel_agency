f0222932461ed58344c72dcd5bf2e69363105f31
jony166091 <israfilhossain166091@gmail.com>
2022-08-09 22:52:32 +0600

    Travel Agency Frontend Task

---
+++ Frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../../imgs/logo.png'
+import './Sidebar.css'
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+        {
+            /*logo*/
+        }
+        <div className="logo">
+            <img src={Logo} alt="logo"/>
+            <span>
+                
+            </span>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
